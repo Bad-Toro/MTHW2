6943a42217910f2aecb76e61c54b3ed688bfe6a5
ferna <fernando.toro@mobileappscompany.com>
2017-04-26 11:11:34 -0400

    Initial commit

---
+++ app/src/main/java/com/mobileappscompany/training/hw2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mobileappscompany.training.hw2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MAV_Tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnP(View view) {
+
+        Intent i = new Intent(this, Main3Activity.class);
+        startActivity(i);
+    }
+}
